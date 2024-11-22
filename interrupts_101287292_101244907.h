eba35da497585b782e761ea8f2b2dda1e92ecb74
mbouc36 <mbouc36@gmail.com>
2024-11-22 14:03:43 -0500

    remove unnecessary files and created memory and process tables

--- interrupts_101287292_101244907.h
+++ interrupts_101287292_101244907.h
@@ -5,11 +5,12 @@
 #include <stdlib.h> 
 
 
+
 typedef struct Partition
 {
-     int PartitionNum; // partitioned number 
-     int size; // size of the memory allowed
-    char code[30]; // string, only use free, init, and other program name
+    int PartitionNum; // partitioned number 
+    int size; // size of the memory allowed
+    int PID; // string, only use free, init, and other program name
 }Partition;
 
 
@@ -19,7 +20,8 @@ typedef struct Process
     int pid; // Process identifier
     int partition_num; // partitioner number
     int size;
-    char name[20];
+    char state[30];
+
 }Process;
 
 typedef struct Program {
@@ -27,63 +29,24 @@ typedef struct Program {
     int memory_size;
 } Program;
 
-//switches user modes (1 ms)
-void switch_user_modes(int *current_time, FILE *execution_file);
 
-//saves and restores context (1-3 ms random)
-void save_restore_context(int *current_time, FILE *execution_file); 
-
-// calculates where in memory the ISR starts (1ms)
-void get_ISR_start_address(int *current_time,int vector_table_address, FILE *execution_file);
-
-//get the address if the ISR from the vector table (1 ms)
-void load_vector_address_to_pc(int *current_time, int ISR_address, FILE *execution_file);
-
-//execute the instructions for the ISR (duration found from input on vector table)
-void execute_FORK(int *current_time, int fork_duration, FILE *execution_file);
-
-//Return the value from the interrupt subroutine ( 1ms)
-void IRET(int *current_time, FILE *execution_file);
-
-//Handles the end of I/O situation
-void end_of_IO(int *current_time, int ISR_duration, FILE *execution_file);
-
-//checks interrupt pritority
-void check_priority_of_ISR(int *current_time, FILE *execution_file);
-
-//check if ISR is masked
-void check_if_masked(int *current_time, FILE *execution_file);
-
-//represetns efforts by the cpu, duration obtained from trace.txt
-void cpu_execution(int *current_time, int duration, FILE *execution_file);
 
 //setups functions to preapre for reading
 void setup_read_file(FILE **filename,  const char *path);
 
-//main function from Assignemnt 1
-void handle_interrupts(FILE *input_file,  FILE *execution_file, FILE *system_status, int *current_time, int *PID, Partition* partitions_array[6], Program* list_of_external_files[5], Process* PCB[6],int vector_table_array[]);
-//what do we do when theres a fork
-void handle_fork(int *current_time, int duration, FILE *output_file);
-
-//What do we di when theres an exec
-void handle_exec(int *current_time, int duration, FILE *output_file, char *program_name, int size);
-
-//use the find the memory size of a given Process in the list of external files
-int find_process_size(Program* list_of_external_files[5], char program_name [20]);
-
 //Used to find an appropriate partition for a program of given size
-int find_partition(int size, char program_name [20],  Partition* partitions_array[6]);
+int find_partition(int size, int PID,  Partition* partitions_array[6]);
 
 //creates Memory Partitions
-Partition* create_partition( int number,  int size, const char *code);
+Partition* create_partition( int number,  int size);
 
 //createds a new Process to be loaded on PCB
-Process* create_new_process( int pid_num, int partition_number,  int size, const char name [20]);
+Process* create_new_process( int pid_num, int partition_number,  int size);
 
-//what happens when we encounter a fork
-void handle_fork(int *current_time, int duration, FILE *output_file);
+//update the state of processes table
+void update_execution(FILE *execution_file, Process *process, char const old_state[20], int current_time);
 
-//prints the current PCB in system status
-void update_PCB(FILE *system_status, Process* PCB[6], int PID,  int current_time);
+//updated the memory state table 
+void update_memory_status(FILE *memory_status, Partition* partitions_array[6], int current_time, int size_used, int *usable_memory);
 
 #endif
