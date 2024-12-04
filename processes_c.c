a54da7d3fe5a89cefd848ef55b0257832e268835
NashM21 <manashaabhayaratne@cmail.carleton.ca>
2024-12-04 15:42:54 -0500

    Ta tries locking the semaphore nad try to lock the nest one too but if not manages TA must let go of the lock he initially got.
    made changes when accessing.

--- processes_c.c
+++ processes_c.c
@@ -10,7 +10,8 @@
 #include <sys/sem.h>
 
 #define TA_NUM 5
-#define STUDENTS_NUM 80
+#define STUDENTS_NUM 20
+#define MAX_ITERATION 3
 
 
 int student_list[STUDENTS_NUM];
@@ -18,17 +19,10 @@ char buffer[250];
 
 typedef struct{
     int students[STUDENTS_NUM];
+    sem_t sem[TA_NUM]; // semaphore for each TA
+    int current_TA; // Want to make sure TAs go in order
+    int marked_count; // tracks marked students so TAs won't repeat
 }Shared_Mem;
-
-void semaphore_init(sem_t *sem){ // wrapper function to initialize all 5 processes 
-    for(int i=0; i< TA_NUM; i++){
-
-        if(sem_init(&sem[i], 0, 1)<0){ // sem_int part of POSIX sempahore API that initializes a semaphore 
-            perror("semaphore_init falied");
-            exit(1);
-        }
-    }
-}
 
 // read from the data file 
 void read_data_file(Shared_Mem *shared_memory){
@@ -55,74 +49,102 @@ void read_data_file(Shared_Mem *shared_memory){
     fclose(student_data);
 }
 
-void TA_Marking(int TA_id, Shared_Mem *shared_memory, sem_t * sem){
+// semaphore init
+void semaphore_init(Shared_Mem *shared_memory) {
+    // Initialize semaphores (one for each TA)
+    for (int i = 0; i < TA_NUM; i++) {
+        if (sem_init(&shared_memory->sem[i], 1, 1) < 0) {  // Shared memory
+            perror("sem_init failed");
+            exit(1);
+        }
+    }
+    shared_memory->current_TA = 0;
+    shared_memory->marked_count = 0;
+}
+
+void TA_Marking(int TA_id, Shared_Mem *shared_memory){
 
     int count_times = 0;
-    int student_id = -1;
+    while(count_times<MAX_ITERATION){
 
+        while(shared_memory->current_TA != TA_id){ // make sure TAs come in order
+            usleep(1000); // short delay to avoid busy wait
+        }
 
-    while(count_times<3){
-        if (sem_wait(&sem[TA_id])== 0){
+        if (sem_wait(&shared_memory->sem[TA_id])== 0){
+            if (sem_trywait(&shared_memory->sem[(TA_id+1)%TA_NUM])==0){
 
-            if (sem_trywait(&sem[(TA_id+1)%TA_NUM])==0){
+                int student_id = -1;
 
-            // start the from the first student of the array in the shared memory
+                // start the from the first student of the array in the shared memory
                 for(int i = 0; i<STUDENTS_NUM;i++){
-                    if(shared_memory->students[i] == 0000){
-                        student_id = i;
-                        printf("TA %d: student id %d aquired\n", TA_id,student_id);
+                    if(shared_memory->students[i] != 0){
+                        student_id = shared_memory->students[i];
+                        shared_memory->students[i] = 0; // mark as taken
+                        shared_memory->marked_count++; 
                         break;
+                        // printf("TA %d: student id %d aquired\n", TA_id + 1,student_id);
+                        // break;
                     }
                 }
+                // reset student list of all students are marked 
+                if (shared_memory->marked_count>= STUDENTS_NUM){
+                    for (int i =0; i<STUDENTS_NUM;i++){
+                        shared_memory->students[i] = i+1;
+                    }
+                    shared_memory->marked_count = 0;
+                    printf("Reset student list\n");
 
-                if (student_id == -1){
-                    break; 
-                }  
+                }
+                sem_post(&shared_memory->sem[(TA_id + 1) % TA_NUM]); // Release next semaphore
+                sem_post(&shared_memory->sem[TA_id]); 
 
-                sleep(rand() % 4+1);
+                if(student_id != -1){
+                    sleep(rand()% 4+1);
 
-            // mark the students
-                int mark = rand() % 10 +1;
-                printf("TA %d has marked student %d\n", TA_id, student_id);
-                FILE *student_mark = fopen("student_marks.txt","a");
-                if(student_list != NULL){
-                    int mark = rand() % 11; 
-                    fprintf(student_mark," TA %d: Student %d : Mark %d\n", TA_id,student_id,mark);
-                    fclose(student_mark);
+                    //mark
+                    int mark = rand() % 10 +1;
+                    printf("TA %d has marked student %d with mark %d\n", TA_id, student_id,mark);
+
+                    //write to TA files
+                    char filename[20];
+                    sprintf(filename, "TA%d.txt", TA_id+1);
+                    FILE *student_marks = fopen(filename,"a");
+                    if (student_marks){
+                        fprintf(student_marks, "TA %d: Student %d: Mark %d \n", TA_id,student_id,mark);
+                        fclose(student_marks);
+                    }else{
+                        printf("Failed to open file\n");
+                    }
+
+                    //marking process delay
+                    sleep(rand()%4+1);
+                    count_times++;
                 }
 
-                sleep(rand()%4+1);
-
-                sem_post(&sem[(TA_id+1)%TA_NUM]);
-                sem_post(&sem[TA_id]);
-
-                count_times++;
-
-
             }else{
-                sem_post(&sem[TA_id]); // is the second semaphore not availbale let go of the first one
+                sem_post(&shared_memory->sem[TA_id]); // is the second semaphore not availbale let go of the first one
+                usleep(rand() % 500 + 100);
             }
         }
-        sleep(rand()%4+1);
-          
+        shared_memory->current_TA = (shared_memory->current_TA + 1) % TA_NUM; // Move to next TA    
     }
 }
+
 
 int main(){
 
     srand(time(NULL));
     Shared_Mem *shared_memory;
-    sem_t sem[TA_NUM]; // array of sempahores
-   
 
      //create shared memory
-    int shm_id = shmget(IPC_PRIVATE, sizeof(Shared_Mem), IPC_CREAT|0666);
-    if(shm_id <0){
+    int shem_id = shmget(IPC_PRIVATE, sizeof(Shared_Mem), IPC_CREAT|0666);
+    if(shem_id <0){
         perror("shmget faild");
         exit(1);
     }
     //attach the shared memory
-    shared_memory = (Shared_Mem *)shmat(shm_id,NULL,0 );
+    shared_memory = (Shared_Mem *)shmat(shem_id,NULL,0 );
     if (shared_memory == (void*)-1){ // if the function fails it retirns (void*)-1, otherwise void* (a pointer to shared mem region )
         perror("shamt failed");
         exit(1);
@@ -130,16 +152,15 @@ int main(){
 
     //initialize student array to 0 
 
-    for(int i = 0; i<STUDENTS_NUM;i++){
-        shared_memory->students[i] =0; 
-    }
+    memset(shared_memory->students, 0, sizeof(shared_memory->students));
 
     //read the txt file into shared memory
 
     read_data_file(shared_memory);
 
     //initialize the semaphores
-    semaphore_init(sem);
+    
+    semaphore_init(shared_memory);
 
     // for the processes
     pid_t pid;
@@ -147,7 +168,7 @@ int main(){
         pid = fork();
         if(pid == 0){
             // child process TA
-            TA_Marking(i,shared_memory,sem);
+            TA_Marking(i,shared_memory);
             exit(0);
         } else if (pid <0){
             perror ("fork failed");
@@ -162,12 +183,8 @@ int main(){
 
     //clean shared memory
     shmdt(shared_memory);
-    shmctl(shm_id, IPC_RMID, NULL);
+    shmctl(shem_id, IPC_RMID, NULL);
 
-    // destroy sempahores
-    for(int i =0 ; i< TA_NUM; i++){
-        sem_destroy(&sem[i]);
-    }
     return 0;
 }
 
